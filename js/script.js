23ff6753775a517d0c2cfd8a48d298e604b7edc4
MarwanSH98 <130684957+MarwanSH98@users.noreply.github.com>
2025-11-06 13:38:00 +0100

    Berufserfahrung hinzufügen

--- js/script.js
+++ js/script.js
@@ -20,16 +20,11 @@ function toggleDarkMode() {
 }
 
 function loadThemePreference() {
-  if (
-    localStorage.getItem("color-theme") === "dark" ||
-    (!("color-theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
+  const saved = localStorage.getItem("color-theme") || "dark";
+  if (saved === "dark") document.documentElement.classList.add("dark");
+  else document.documentElement.classList.remove("dark");
 }
+
 
 // ========== MOBILE MENU TOGGLE ==========
 function toggleMobileMenu() {
